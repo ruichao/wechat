ae494139c59fe1ce8b8ccc5308431cabc4a721d3
Ruichao Xue <ruichaoxue@gmail.com>
2018-02-01 02:00:05 +0800

    implement todos app

--- pages/todos/todos.js
+++ pages/todos/todos.js
@@ -1,37 +1,58 @@
 Page({
   data: {
-    todos: [{
-      name: 'buy milk',
-      state: 'finished',
-      id: 1
-    }, {
-      name: 'read js',
-      state: 'active',
-      id: 2
-    }],
+    todos: [],
     filter: 'all',
     inputName: '',
     currentId: 3
   },
+  onLoad: function() {
+    this.setData({
+      todos: wx.getStorageSync('todos') || [],
+      filter: wx.getStorageSync('filter') || 'all' ,
+      currentId: wx.getStorageSync('currentId') || 1
+    })
+  },
+  onUnload: function() {
+    wx.setStorageSync('todos', this.data.todos);
+    wx.setStorageSync('filter', this.data.filter);
+    wx.setStorageSync('currentId', this.data.currentId);
+  },
   addTodo: function(e) {
     if (this.data.inputName.trim().length > 0) {
-      const todos = this.data.todos.push({
-        type: 'active',
-        name: this.data.inputName,
-        state: 'active',
-        id: this.currentId++
-      });
       this.setData({
-        todos: todos,
+        todos: [...this.data.todos, {
+          name: this.data.inputName,
+          state: 'active',
+          id: this.data.currentId++
+        }],
         inputName: ''
       })
     }
   },
+  deleteTodo: function(e) {
+    const id = e.target.dataset.id;
+    this.setData({
+      todos: this.data.todos.filter((todo) => todo.id !== id)
+    });
+  },
+  toggleState: function(e) {
+    this.setData({
+      todos: this.data.todos.map((todo) => {
+        if (todo.id === e.target.dataset.id) {
+          return {...todo, state: todo.state === 'active' ? 'finished' : 'active'}
+        }
+        return todo;
+      })
+    });
+  },
   bindInput: function(e) {
-    console.log(e)
-    console.log(e.detail.value)
     this.setData({
       inputName: e.detail.value
-    })
+    });
+  },
+  changeFilter: function(e) {
+    this.setData({
+      filter: e.target.dataset.filter
+    });
   }
 })
